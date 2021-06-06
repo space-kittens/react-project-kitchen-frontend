15b824fbda7034a35222fa2d7bc3d6325e6e721a
Igor Zorin <igor_zorin@mail.ru>
2021-06-06 14:35:48 +0300

    добавляет глобальные стили, browserslist, node-sass, подключает шрифты

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import React from 'react';
-import { store, history} from './store';
+import { store, history } from './store';
 
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
+
+import './scss/styles.scss';
 
 import App from './components/App';
 
