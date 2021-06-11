ad4350f42e02113566cc1b4645cc8d1c8fbe608a
pdrobyshev <pdrobyshev@wooppay.com>
2021-06-11 19:08:14 +0600

    Раскидал компоненты Article по своим папкам, прошёлся в каждом файле prettier'ом

--- src/components/Article/ArticleMeta/ArticleMeta.js
+++ src/components/Article/ArticleMeta/ArticleMeta.js
@@ -1,22 +1,20 @@
-import ArticleActions from './ArticleActions';
+import ArticleActions from '../ArticleActions/ArticleActions';
 import { Link } from 'react-router-dom';
 import React from 'react';
 
-const ArticleMeta = props => {
+const ArticleMeta = (props) => {
   const article = props.article;
   return (
-    <div className="article-meta">
+    <div className='article-meta'>
       <Link to={`/@${article.author.username}`}>
         <img src={article.author.image} alt={article.author.username} />
       </Link>
 
-      <div className="info">
-        <Link to={`/@${article.author.username}`} className="author">
+      <div className='info'>
+        <Link to={`/@${article.author.username}`} className='author'>
           {article.author.username}
         </Link>
-        <span className="date">
-          {new Date(article.createdAt).toDateString()}
-        </span>
+        <span className='date'>{new Date(article.createdAt).toDateString()}</span>
       </div>
 
       <ArticleActions canModify={props.canModify} article={article} />
