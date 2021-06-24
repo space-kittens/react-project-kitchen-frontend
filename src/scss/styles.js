48a2f2974b6901cb1d038c208cf4168db1295a46
pdrobyshev <pdrobyshev@wooppay.com>
2021-06-24 17:39:48 +0600

    редизайн шапки

---
+++ src/scss/styles.js
@@ -0,0 +1,46 @@
+export const colorDefault = {
+  black: '#0000000',
+  white: '#ffffff',
+  transparent: 'rgba(255, 255, 255, 0)',
+};
+
+export const colorBase = {
+  input: '#2F2F37',
+  accent: '#4C4CFF',
+  divider: '#2F2F37',
+  alert: '#F20D33',
+  success: '#00CCCC',
+  disabled: '#3A3A55',
+};
+
+export const colorBackground = {
+  primary: '#131316',
+  secondary: '#1C1C21',
+};
+
+export const colorText = {
+  primary: '#F2F2F3',
+  secondary: '#8585AD',
+};
+
+export const fontFamily = {
+  jetBrains: '"JetBrains Mono", "Courier", monospace',
+  exo2: '"Exo 2", "Arial", sans-serif',
+};
+
+export const animation = {
+  defaultTransition: '0.3s ease',
+};
+
+export const viewports = {
+  sm: '320px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1440px',
+  xxl: '1920px',
+};
+
+export const retina = {
+  dpi: '144dpi',
+  dppx: '1.5dppx',
+};
